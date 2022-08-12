a9966795b7ccfe80fab0b9025bba1c861882aeaa
rachel-o-donnell <hellotherachelo@gmail.com>
2022-08-12 15:29:13 +0000

    Styled dropdown button to suit my project and add credit comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-/* dropdown code taken from w3schools */ 
+// dropdown code taken from w3schools adapted to suit my project
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// When the user clicks on the button,toggle between hiding and showing the dropdown content W3schools
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  // Close the dropdown menu if the user clicks outside of it
+  // Close the dropdown menu if the user clicks outside of it W3schools 
 window.onclick = function(event) {
     if (!event.target.matches('.dropdown-btn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
