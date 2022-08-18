6efb46116b7389537d6e53503d3c93e8fac584bd
rachel-o-donnell <hellotherachelo@gmail.com>
2022-08-18 10:45:53 +0000

    Add initial function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 // dropdown code taken from w3schools adapted to suit my project
 
 // When the user clicks on the button,toggle between hiding and showing the dropdown content W3schools
-/* function showDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+function showDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-  // Close the dropdown menu if the user clicks outside of it W3schools
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-btn')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// Close the dropdown menu if the user clicks outside of it W3schools
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown-btn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-  
+}
+
 // choosing game difficulty 
 /*
  not sure if this will work to rename the button but this was my attempt but I can use it to fetch the api 
@@ -39,33 +39,28 @@ function selectDifficulty() {
 
 // displaying game difficulty 
 
-/* option.addEventListener('click', displaySelectedOption() array.forEach(element => {
-	{
-}); 
-  let option = document.getElementsByClassName('option');
-  let dropdownBtnText = document.getElementById('dropdown-btn').textcontent;
-  
+
+/*" keep getting error messages that either OPTION IS NOT DEFINED/ addevent listener is not a function  "
+let option = document.getElementsByTagName('a');
+let dropdownBtnText = document.getElementById('dropdown-btn').textcontent;
+
   function displaySelectedOption() {
     if (option.textContent === "Easy") {
-    dropdownBtnText = 'Difficuly: Easy'
-  }
-    else if (option.textContent === "Medium") {
+      dropdownBtnText = 'Difficuly: Easy'
+    } else if (option.textContent === "Medium") {
       dropdownBtnText = "Difficuly: Medium"
+    } else if (option.textContent === "Hard") {
+      dropdownBtnText = "Difficuly: Hard"
     }
-     else if (option.textContent === "Hard") {
-        dropdownBtnText = "Difficuly: Hard"
-      }
-    }
-  });
+  }
+  option.addEventListener('click', displaySelectedOption())
 
+*/
 
-  // hides home screen when start button is pressed and replaces with game screen
+// hides home screen when start button is pressed and replaces with game screen
 
 /* addEventListeners(click)
 function startGame() {
     document.getElementsByClassName('hide').style="display:none";
-    console.log('game started')
-  } */ 
-  function myFunction() {
-    document.getElementById("mySelect").value = "banana";
-  }
+    console.log('game started') 
+ } */
