487c934c743955a456a6a33c9ba4977cd3339780
rachel-o-donnell <hellotherachelo@gmail.com>
2022-09-02 09:11:26 +0000

    Tidy script.js, add comments to all functions and notes to myself for next steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,37 +15,45 @@ let score = document.getElementById('score');
 let questionCountDisplay = document.getElementById('question-count-display');
 let questionText = document.getElementById('question');
 let answerButtons = document.getElementsByClassName('ans-btn');
-let randomQuestion, chosenDifficulty;
 let level;
+let chosenDifficulty;
 let correct, wrong;
-//let currentQuestionIndex = 0;
-let currentGameQuestions = [];
 let ansA = document.getElementById('a');
 let ansB = document.getElementById('b');
 let ansC = document.getElementById('c');
 let ansD = document.getElementById('d');
 let quizLength = 8;
 let endMessage = document.getElementById('end-message');
-let selectedAns;
+let correctAns;
+//let selectedAns;
+//let randomQuestion
+//let currentQuestionIndex = 0;
+//let currentGameQuestions = [];
+
 
 // EVENT LISTENERS
-beginBtn.addEventListener('click', beginGame);
-document.getElementById('home.btn').addEventListener('click', goHome);
-// PLAY AGAIN BUTTON - RESETS THE GAME TO THE START
-playAgain.addEventListener('click', goHome);
 
-ansA.addEventListener('click', checkAns);
-ansB.addEventListener('click', checkAns);
+beginBtn.addEventListener('click', beginGame); // BEGIN BUTTON- BEGINS THE GAME
+document.getElementById('home.btn').addEventListener('click', goHome); // HOME ICON - RESETS TO HOME SCREEN
+playAgain.addEventListener('click', goHome); // PLAY AGAIN BUTTON - RESETS THE GAME TO THE START
+
+// !!!!!!  NEED TO DISABLE THESE WHEN ONE IS CLICKED AND ONLY ALLOW NEXT BUTTON TO BE PRESSES WHEN ANSWERED . ADD HOVER? !!!!! 
+ansA.addEventListener('click', checkAns); // ANSWER BUTTONS - CHECKS IF THE SELECTED ANSWER IS CORRECT OR WRONG
+ansB.addEventListener('click', checkAns); 
 ansC.addEventListener('click', checkAns);
 ansD.addEventListener('click', checkAns);
-//begins game - hides home container, shows game screen, sets username in game panel, sets score to zero
 
+
+
+// SETS USERNAME IN PANEL WHEN CALLED
 function setUsername() {
     let name = document.getElementById('name');
     let username = name.value;
-    document.getElementById('username').textContent = username
+    document.getElementById('username').textContent = username;
 }
 
+
+// BEGINS GAME - HIDES HOME CONTAINER , SHOWS GAME SCREEN, DISPLAYS NAME, RESETS SCORE AND QUESTION COUNTER
 function beginGame() {
 
     for (let elements of homeScreenElements) {
@@ -60,6 +68,7 @@ function beginGame() {
 
 }
 
+// RESETS COLOUR OF ANSWER BUTTONS
 
 function resetAnsBtnColor() {
     for (let i = 0; i < answerButtons.length; i++) {
@@ -67,15 +76,16 @@ function resetAnsBtnColor() {
     }
 }
 
+
+// IDENTIFIES USERS CHOSEN DIFFICULTY FROM DROPDOWN AND LOADS AND RANDOMISES CORRESPONDING CATEGORY
 document.getElementById('select').addEventListener('change', function() {
   chosenDifficulty = this.value;
   console.log(chosenDifficulty);
+
   if (chosenDifficulty === 'easy') {
     chosenDifficulty = easyCategory;
     level = 'easy';
     shuffle(easyCategory);
-    console.log('SHUFFLE EASY');
-    console.log(easyCategory);
     chooseNextQuestion();
   }
 
@@ -83,8 +93,6 @@ document.getElementById('select').addEventListener('change', function() {
     chosenDifficulty = mediumCategory;
     level = 'medium';
     shuffle(mediumCategory);
-    console.log('SHUFFLE MEDIUM');
-    console.log(mediumCategory);
     chooseNextQuestion();
   } 
 
@@ -92,13 +100,11 @@ document.getElementById('select').addEventListener('change', function() {
     chosenDifficulty = hardCategory;
     level = 'hard';
     shuffle(hardCategory);
-    console.log('SHUFFLE HARD');
-    console.log(hardCategory);
     chooseNextQuestion();
   } 
 });
 
-/*
+/* ORIGINAL SHUFFLE 
 function shuffle() {
   randomQuestion = chosenDifficulty.sort(() => Math.random() - .5); 
 }
@@ -106,6 +112,7 @@ function shuffle() {
 
 // Fisher yates shuffle code from Sean young on Slack. Took a few attempts to understand and how to apply to my needs
 
+// SHUFFLES QUESTIONS OF WHATEVER CATERGORY IS CHOSEN. 
 function shuffle(questions) {
     for (let i = questions.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -116,9 +123,9 @@ function shuffle(questions) {
 
 }
 
-
+// 
 function chooseNextQuestion() {
-  let currentQuestionIndex = 0;
+  let currentQuestionIndex = 0; 
 /*  for (let i = 0, i <== questions.length, i++) {
     console.log(questions)
   }
@@ -129,7 +136,6 @@ function chooseNextQuestion() {
   //currentQuestionIndex ++
   //question++
 
-
   /*
   tutor support :
   chooseNextQuestion(currentQuestionIndex);
@@ -139,12 +145,25 @@ function chooseNextQuestion() {
  */
 }
 
+// NEXT BUTTON RESETS COLOR, INCREASES QUESTION COUNTER
+
+// ------- !!!NEED TO GET IT TO CALL NEXT QUESTION AND ANSWER SET!!!
+nextButton.addEventListener('click', function() {
+    resetAnsBtnColor();
+    //shuffle(easyCategory); ---- THIS JuST SHUFFLES THE CATEGORY EACH TIME SO CAN END UP WITH SAME QUESTION IN THE GAME
+   
+    chooseNextQuestion();
+    questionOfQuestion();
+  });
+  
+
+// DISPLAYS QUESTION AND ANSWERS 
 function displayQuestion(question) {
   questionText.innerHTML = question.question;
   //displays answers
   question.answers.forEach((answer, index) => {
       answerButtons[index].innerHTML = answer;
-  })
+  });
 
   correctAns = question.correct_answer;
     console.log(correctAns);
@@ -152,6 +171,7 @@ function displayQuestion(question) {
 }
     //selectedAns = this.innerHTML
 
+// CHECKS ANSWERS AND CHANGES COLOR OF SELECTED BUTTONS TO RED OR GREEN ACCORDINGLY
 function checkAns() {
 
   console.log('you selected ' + this.innerHTML);
@@ -169,12 +189,83 @@ function checkAns() {
 }
 //}
 
-nextButton.addEventListener('click', function() {
-  resetAnsBtnColor();
-  //shuffle(easyCategory);
-  chooseNextQuestion();
-  questionOfQuestion();
-});
+// GAME PANEL BAR 
+
+// Home button pressed - goes to ghome screen - 
+
+// RESETS GAME TO HOME SCREEN WHEN PRESSED 
+// !!!! DO YOU WANT TO CLEAR THE USERNAME? OR KEEP IT? !!!!
+// !!!! DO YOU WANT TO ADD A WARNING? !!!!!
+
+function goHome() {
+    gameScreenElements.classList.add('hide-game');
+    for (let elements of homeScreenElements) {
+        elements.classList.remove('hide-home');
+    }
+    homeContainer.removeAttribute('style');
+    gameScreenContainer.removeAttribute('style');
+    gameContainer.classList.remove('hide-game');
+    endGameContainer.classList.add('hide');
+    endGameContainer.removeAttribute('style');
+    /* for (let elements of endGameContainer) {
+       elements.classList.add('hide');
+     } */
+}
+
+
+// INSCREASES SCORE 
+function adjustScore() {
+    let oldScore = parseInt(score.innerText);
+    score.innerText = ++oldScore;
+}
+
+// DISPLAYS WHAT QUESTION YOU ARE ON OUT OF HOW MANY AND INCREASES WHEN CALLED BY NEXT BUTTON
+function questionOfQuestion() {
+    console.log('questionCountDisplay');
+    let questionCount = parseInt(questionCountDisplay.innerText);
+    questionCountDisplay.innerText = ++questionCount;
+}
+
+// CHECKS THE QUESTION COUNT AND CALLS TO END THE GAME WHEN IT REACHES 9 
+function queQuestionCount() {
+    if (questionCountDisplay.innerHTML >= quizLength) {
+        console.log('queQuestionCount');
+        endGame();
+    }
+        //chooseNextQuestion();
+   // } else {
+   //     endGame();
+    //}
+}
+
+// HIDES GAME SCREEN AND DISPLAYS END GAME SCREEN, 
+// DISPLAYS A CUSTOM MESSAGE WITH YOUR SCORE AND CHOSEN DIFFICULTY LEVEL,
+// DISPLAYS A DIFFERENT PAINTING WITH A MYTHOLOGICAL THEME DEPENDING ON LEVEL CHOSEN. 
+
+function endGame() {
+    gameScreenElements.classList.add('hide-game');
+    gameContainer.classList.add('hide-game');
+    endScreen.classList.remove('hide');
+    gameScreenContainer.style.removeProperty('display');
+    endGameContainer.style.display = 'flex';
+    //endMessage()
+    endMessage.innerHTML = (`You scored ${score.innerText} out of 8 in the ${level} category. Ready to play again?`);
+    if (level === 'easy') {
+        venus.classList.remove('hide');
+    }
+    if (level === 'medium') {
+        perseus.classList.remove('hide');
+    }
+    if (level === 'hard') {
+        oedipus.classList.remove('hide');
+       
+    }
+}
+
+
+
+// OLD CODE FOR DISPLAYING Q&A _ WORKED BUT ALSO WAS TERRIBLY STRUCTURED AND WOULD REPEAT CALLS ON CONSOLE> 
+
 
 //function chooseNextQuestion() {
 // displayQuestions(category[index]);
@@ -276,81 +367,4 @@ if (chosenDifficulty === 'medium') {
     }
 }
 }) //
-
-*/ 
-// GAME PANEL BAR 
-
-// Home button pressed - goes to ghome screen - DO YOU WANT TO CLEAR THE USERNAME? OR KEEP IT? 
-
-
-function goHome() {
-    gameScreenElements.classList.add('hide-game');
-    for (let elements of homeScreenElements) {
-        elements.classList.remove('hide-home');
-    }
-    homeContainer.removeAttribute('style');
-    gameScreenContainer.removeAttribute('style');
-    gameContainer.classList.remove('hide-game');
-    endGameContainer.classList.add('hide');
-    endGameContainer.removeAttribute('style');
-    /* for (let elements of endGameContainer) {
-       elements.classList.add('hide');
-     } */
-}
-
-function adjustScore() {
-    let oldScore = parseInt(score.innerText);
-    score.innerText = ++oldScore;
-}
-
-function questionOfQuestion() {
-    console.log('questionCountDisplay');
-    let questionCount = parseInt(questionCountDisplay.innerText);
-    questionCountDisplay.innerText = ++questionCount;
-}
-
-function queQuestionCount() {
-    if (questionCountDisplay.innerHTML >= quizLength) {
-        console.log('queQuestionCount');
-        endGame();
-    }
-        //chooseNextQuestion();
-   // } else {
-   //     endGame();
-    //}
-}
-
-function endGame() {
-    gameScreenElements.classList.add('hide-game');
-    gameContainer.classList.add('hide-game');
-    endScreen.classList.remove('hide');
-    gameScreenContainer.style.removeProperty('display');
-    endGameContainer.style.display = 'flex';
-    //endMessage()
-    endMessage.innerHTML = (`You scored ${score.innerText} out of 8 in the ${level} category. Ready to play again?`);
-    if (level === 'easy') {
-        venus.classList.remove('hide');
-    }
-    if (level === 'medium') {
-        perseus.classList.remove('hide');
-    }
-    if (level === 'hard') {
-        oedipus.classList.remove('hide');
-       
-    }
-}
-
-/*
-function endMessage() {
-endMessage.innerHTML = (`You scored ${score.innerText} out of 8 in the ${chosenDifficulty} category. Ready to play again?`);
-    if (chosenDifficulty === 'easy') {
-        venus.classList.remove('hide');
-    }
-    if (chosenDifficulty === 'medium') {
-        perseus.classList.remove('hide');
-    }
-    if (chosenDifficulty === 'hard') {
-        oedipus.classList.remove('hide');
-    }
-  }
-  */
+*/
